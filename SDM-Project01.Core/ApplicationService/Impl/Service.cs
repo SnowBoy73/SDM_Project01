6b10abbaf0873ec2c4374eba854be583ea8be685
SnowBoy73 <56825266+SnowBoy73@users.noreply.github.com>
2020-09-27 20:10:04 +0200

    GetNumberOfReviewsFromReviewer written

--- SDM-Project01.Core/ApplicationService/Impl/Service.cs
+++ SDM-Project01.Core/ApplicationService/Impl/Service.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using SDM_Project01.Core.DomianService;
+using SDM_Project01.Core.Entity;
 
 namespace SDM_Project01.Core.ApplicationService.Impl
 {
@@ -16,72 +17,77 @@ namespace SDM_Project01.Core.ApplicationService.Impl
 
         public int GetNumberOfReviewsFromReviewer(int reviewer)
         {
-            return 0;
+            List<Review> result = new List<Review>();
+            foreach (Review r in _repo.GetAllReviews())
+                if (r.ReviewerId == reviewer)
+                    result.Add(r);
+            return result.Count();
         }
+
 
         public double GetAverageRateFromReviewer(int reviewer)
         {
             return 0;
-
         }
+
 
         public int GetNumberOfRatesByReviewer(int reviewer, int rate)
         {
             return 0;
-
         }
+
 
         public int GetNumberOfReviews(int movie)
         {
             return 0;
-
         }
+
 
         public double GetAverageRateOfMovie(int movie)
         {
             return 0;
-
         }
+
 
         public void GetNumberOfReviewsFromReviewer(object reviewer)
         {
             throw new NotImplementedException();
         }
 
+
         public int GetNumberOfRates(int movie, int rate)
         {
             return 0;
-
         }
+
 
         public List<int> GetMoviesWithHighestNumberOfTopRates()
         {
             return null;
-
         }
+
 
         public List<int> GetMostProductiveReviewers()
         {
             return null;
-
         }
+
 
         public List<int> GetTopRatedMovies(int amount)
         {
             return null;
-    
         }
+
 
         public List<int> GetTopMoviesByReviewer(int reviewer)
         {
             return null;
-
         }
+
 
         public List<int> GetReviewersByMovie(int movie)
         {
             return null;
-
         }
 
     }
